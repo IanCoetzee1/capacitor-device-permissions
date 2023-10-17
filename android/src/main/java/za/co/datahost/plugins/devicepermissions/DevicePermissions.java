30624af2d42460f6c5abb1200563a3bf2f13a9e3
Ian Coetzee <ian@webmagic.co.za>
2023-10-17 17:56:32 +0200

    Minor spacing change

--- android/src/main/java/za/co/datahost/plugins/devicepermissions/DevicePermissions.java
+++ android/src/main/java/za/co/datahost/plugins/devicepermissions/DevicePermissions.java
@@ -8,7 +8,7 @@ public class DevicePermissions {
 
     public String echo(String value) {
         //Log.i("Echo", value);
-        Log.i("Echo", "I added this" + value);
+        Log.i("Echo", "I added this: " + value);
         return value;
     }
     public void setup(JSObject config) {
