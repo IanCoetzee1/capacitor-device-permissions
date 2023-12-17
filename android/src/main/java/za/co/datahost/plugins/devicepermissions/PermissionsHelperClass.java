63ade2215be0d52b10a7b20b1422fab68c527400
Ian Coetzee <ian@webmagic.co.za>
2023-12-17 21:40:52 +0200

    Updated helper file

--- android/src/main/java/za/co/datahost/plugins/devicepermissions/PermissionsHelperClass.java
+++ android/src/main/java/za/co/datahost/plugins/devicepermissions/PermissionsHelperClass.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 
 public class PermissionsHelperClass {
 
-  public Context context;
+  private Context context;
 
   public void setContext(Context context) {
     this.context = context;
