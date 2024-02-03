21f499087a237afd2bb40b0cd642f685151d627b
Ian Coetzee <ian@webmagic.co.za>
2024-02-03 23:08:01 +0200

    Finally figured out how to run through an array

--- android/src/main/java/za/co/datahost/plugins/devicepermissions/PermissionsHelperClass.java
+++ android/src/main/java/za/co/datahost/plugins/devicepermissions/PermissionsHelperClass.java
@@ -1,21 +1,25 @@
 package za.co.datahost.plugins.devicepermissions;
 
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.huawei.hms.api.HuaweiApiAvailability;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import android.content.Context;
 import android.util.Log;
 import com.google.gson.Gson;
 
 public class PermissionsHelperClass {
-
     private Context context;
     private String OSIdent;
-    private Gson permissionsGSON;
+    private JsonElement permissionsJsonAliasElement;
 
     public void setContext(Context context) {
         this.context = context;
@@ -26,19 +30,38 @@ public class PermissionsHelperClass {
     }
 
     public void setDevicePermissionsGson(JSONObject allPermissions) {
-        JSONObject permissionsJSON;
         JSONObject alias;
-
         try {
+
             if (allPermissions.has(this.OSIdent) && allPermissions.getJSONObject(this.OSIdent).has("alias")) {
                 alias = allPermissions.getJSONObject(this.OSIdent);
-                permissionsJSON = alias.getJSONObject("alias");
-                //permissionsGSON.fromJson(permissionsJSON, String);
-                Log.i("Ian/Dev", "JSON: " + permissionsJSON);
+                Gson gson = new Gson();
+                this.permissionsJsonAliasElement = gson.fromJson(alias.getJSONObject("alias").toString() , JsonElement.class);
+                if (this.permissionsJsonAliasElement.isJsonObject()) {
+                    JsonObject jsonObject = this.permissionsJsonAliasElement.getAsJsonObject();
+                    for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                        String key = entry.getKey();
+                        JsonObject MemberEntries = entry.getValue().getAsJsonObject();
+
+                        Integer minSDK = MemberEntries.get("minSDK").getAsInt();
+                        Integer maxSDK = MemberEntries.get("maxSDK").getAsInt();
+                        JsonArray permissionsArray = MemberEntries.get("permissionsArray").getAsJsonArray();
+                        Log.i("Ian/Dev", "Key: " + key + " Value: " + permissionsArray );
+                        for (int i = 0; i < permissionsArray.size(); i++) {
+                            String permission = permissionsArray.get(i).getAsString();
+                            Log.i("Ian/Dev", "   Count: (" + i + ") " + permission);
+                        }
+                    }
+                } else {
+                    Log.e("Plugin: capacitor-device-permissions","Malformed JSON file (supported_permissions.json) class member '" + this.OSIdent + "' does not exist or the alias sub-member is not a valid JSON Object");
+                    throw new RuntimeException("Plugin: capacitor-device-permissions - Malformed JSON file (supported_permissions.json).");
+                }
+                Log.i("Ian/Dev", "JSON: " + this.permissionsJsonAliasElement.toString());
             } else {
                 Log.e("Plugin: capacitor-device-permissions","Malformed JSON file (supported_permissions.json) class member '" + this.OSIdent + "' does not exist or the member does not have an 'alias' sub-member");
                 throw new RuntimeException("Plugin: capacitor-device-permissions - Malformed JSON file (supported_permissions.json).");
             }
+
         } catch (JSONException ex) {
             Log.e("Plugin: capacitor-device-permissions","JSON does not contain a member named " + this.OSIdent);
             ex.printStackTrace();
@@ -57,7 +80,6 @@ public class PermissionsHelperClass {
         int resultCode = apiAvailability.isHuaweiMobileServicesAvailable(context);
         return resultCode == com.huawei.hms.api.ConnectionResult.SUCCESS;
     }
-
     public JSONObject readJSONFile(int resourceId) {
         try {
             Context context = this.context;
